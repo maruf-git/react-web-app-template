172474fdc028f647e70ef58ad429be35a7a606f6
orion <munna.csehstu@gmail.com>
2024-12-12 20:24:10 +0600

    updated routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from './router/router'
+import MainRoute from './routes/MainRoute'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <RouterProvider router={router} />
+     <RouterProvider router={MainRoute} />
   </StrictMode>,
 )
